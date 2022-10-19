6d140402bd60ac9d49924f553ba36c036122cbf7
Wakanda Rajyam <116173131+wakandarajyam@users.noreply.github.com>
2022-10-19 19:32:19 +0530

    Improved readability and fixed errors

--- AbMultiply.java
+++ AbMultiply.java
@@ -1,68 +1,69 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class AbMultiply {
 
-    public static Scanner scn = new Scanner(System.in);
+	public static Scanner scanner = new Scanner(System.in);
 
-    public static int AbAddition(int n1, int n2, int b) {
-        int ans = 0;
-        int pow = 1;
-        int carry = 0;
+	public static int AbAddition(int n1, int n2, int b) {
+		int answer = 0;
+		int power = 1;
+		int carry = 0;
 
-        while (n1 != 0 || n2 != 0 || carry != 0) {
-            int sum = carry + (n1 % 10) + (n2 % 10);
-            n1 /= 10;
-            n2 /= 10;
+		while (n1 != 0 || n2 != 0 || carry != 0) {
+			int sum = carry + (n1 % 10) + (n2 % 10);
+			n1 /= 10;
+			n2 /= 10;
 
-            int digit = sum % b;
-            carry = sum / b;
+			int digit = sum % b;
+			carry = sum / b;
 
-            ans += digit * pow;
-            pow *= 10;
-        }
-        return ans;
-    }
+			answer += digit * power;
+			power *= 10;
+		}
+		return answer;
+	}
 
-    public static int singleDigitMultiply(int n1, int d2, int b) {
-        int ans = 0;
-        int pow = 1;
-        int carry = 0;
+	public static int singleDigitMultiply(int n1, int d2, int b) {
+		int answer = 0;
+		int power = 1;
+		int carry = 0;
 
-        while (n1 != 0 || carry != 0) {
-            int d1 = n1 % 10;
-            n1 /= 10;
+		while (n1 != 0 || carry != 0) {
+			int remainder = n1 % 10;
+			n1 /= 10;
 
-            int d = carry + (d1 * d2);
-            int digit = d % b;
-            carry = d / b;
+			int d = carry + (remainder * d2);
+			int digit = d % b;
+			carry = d / b;
 
-            ans += digit * pow;
-            pow *= 10;
-        }
-        return ans;
-    }
+			answer += digit * power;
+			power *= 10;
+		}
+		return answer;
+	}
 
-    public static int multiplication(int n1, int n2, int b) {
-        int ans = 0;
-        int pow = 1;
+	public static int multiplication(int n1, int n2, int b) {
+		int answer = 0;
+		int power = 1;
 
-        while (n2 != 0) {
-            int d2 = n2 % 10;
-            n2 /= 10;
+		while (n2 != 0) {
+			int remainder = n2 % 10;
+			n2 /= 10;
 
-            int prod = singleDigitMultiply(n1, d2, b) * pow;
-            ans = AbAddition(ans, prod, b);
-            pow *= 10;
-        }
-        return ans;
-    }
+			int product = singleDigitMultiply(n1, remainder, b) * power;
+			answer = AbAddition(answer, product, b);
+			power *= 10;
+		}
+		return answer;
+	}
 
-    public static void main(String[] args) {
-        int n1 = scn.nextInt();
-        int n2 = scn.nextInt();
-        int b = scn.nextInt();
-        int product = multiplication(n1, n2, b);
-        System.out.print(product);
-    }
+	public static void main(String[] args) {
+		int n1 = scanner.nextInt();
+		int n2 = scanner.nextInt();
+		int b = scanner.nextInt();
+		
+		int product = multiplication(n1, n2, b);
+		
+		System.out.print(product);
+	}
 }
- 
